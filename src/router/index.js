4ce74b1eab50c768bb5678705c739935ca94d245
Mr.Zhy <LiZhenShuai@handsome.com>
2018-12-08 23:32:05 +0800

    导入后台文件

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import login from "../page/login.vue"
+import index from "../page/index.vue"
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'login',
-      component: login
-    }
+    {path: '/',redirect:'/login'}, 
+    {path: '/login',name: 'login', component: login},
+    {path: '/index',name: 'index', component: index}
   ]
 })
